a0ac81483ab87c3ace2305f648429a887436c42d
Jesus Romero <jhromero@tekmexico.com>
2021-01-26 14:06:45 -0800

    feat: added checks for expiration

--- src/model.js
+++ src/model.js
@@ -1,6 +1,7 @@
 import { nanoid } from 'nanoid'
 import normalizeURL from 'normalize-url';
 
+const oneday = 60 * 60 * 24 * 1000;
 // CHANGE TO USE A DB FOR A PERSISTENT SOLUTION
 
 class ShortURL {
@@ -9,35 +10,66 @@ class ShortURL {
 
     constructor(url = '') {
         const normalized = normalizeURL(url);
+
         if (ShortURL.cacheURL.has(normalized)) {
-            // check expiration
-            this.url = normalized;
-            this.id = ShortURL.getID(normalized)
-            return;
+            const urlObject = ShortURL.cacheURL.get(normalized);
+            if (!ShortURL.checkExpiration(urlObject)) {
+                urlObject.expire = Date.now() + oneday;
+                return urlObject;
+            }
         }
-        this.url = normalized;
 
         const idLength = 10;
         let id = nanoid(idLength);
-        while (ShortURL.cacheID.has(id)) {
-            // check expiration
-            id = nanoid(idLength);
-        }
-        this.id = id;
 
+        while (ShortURL.cacheID.has(id)) {
+            const urlObject = ShortURL.cacheID.get(id);
+            if (!ShortURL.checkExpiration(urlObject)) {
+                id = nanoid(idLength);
+            }
+        }
+
+        this.url = normalized;
+        this.id = id;
+        this.expire = Date.now() + oneday
         ShortURL.addURL(this);
     }
-    static addURL({ url, id }) {
-        ShortURL.cacheURL.set(url, id);
-        ShortURL.cacheID.set(id, url);
+
+    static addURL(urlObject = { url = '', id = '', expire = Date.now() }) {
+        const { url, id } = urlObject
+        ShortURL.cacheURL.set(url, urlObject);
+        ShortURL.cacheID.set(id, urlObject);
     }
+
     static getURL(id = '') {
-        // check expiration
-        return ShortURL.cacheID.get(id);
+        const urlObject = ShortURL.cacheID.get(id);
+        if (ShortURL.checkExpiration(urlObject) || !urlObject) {
+            return null;
+        }
+        else {
+            urlObject.expire = Date.now() + oneday;
+            return ShortURL.cacheID.get(id).url;
+        }
     }
+
     static getID(url = '') {
-        // check expiration
-        return ShortURL.cacheURL.get(url);
+        const urlObject = ShortURL.cacheURL.get(url);
+        if (ShortURL.checkExpiration(urlObject) || !urlObject) {
+            return null;
+        }
+        else {
+            urlObject.expire = Date.now() + oneday;
+            return ShortURL.cacheURL.get(url).id;
+        }
+    }
+
+    static checkExpiration(urlObject = { url = '', id = '', expire = Date.now() }) {
+        if (urlObject.expire < Date.now) {
+            ShortURL.cacheID.delete(urlObject.id);
+            ShortURL.cacheURL.delete(urlObject.url);
+            return true;
+        }
+        return false;
     }
 }
 
