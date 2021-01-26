1a5e431aec8da3e9e664c7df602fb506a7e7cbb5
Jesus Romero <jhromero@tekmexico.com>
2021-01-26 10:16:23 -0800

    refactor: renamed app to view

---
+++ src/view.js
@@ -0,0 +1,14 @@
+const view = (shortURL) =>
+  `<div style="padding: 20px">
+    <h1>URL SHORTENER</h1>
+    <form action='/' method="POST">
+      <input type='text' name='longURL'/>
+      <button>Submit</button>
+    <form>
+   </div>
+  <div>
+    ${shortURL ? `<a href="${shortURL}">${shortURL}</a>` : ''}
+  </div>
+  `
+
+export default view
