8d4e80f6f3e5fff9ff7f9e905e4d630e4eae16ae
Jesus Romero <jhromero@tekmexico.com>
2021-01-26 10:17:51 -0800

    feat: added tests WIP

---
+++ test/functionality_test.js
@@ -0,0 +1,20 @@
+import assert from 'assert'
+
+describe('Functionality testing', () => {
+  it('Should render page correctly', () => {
+      //TODO
+      assert.strictEqual(true, !false)
+    });
+    it('Should return shortened url', () => {
+      //TODO
+      assert.strictEqual(true, !false)
+    });
+    it('Should return same url if page already given', () => {
+      //TODO
+      assert.strictEqual(true, !false)
+    });
+    it('Should redirect to correct page', () => {
+      //TODO
+    assert.strictEqual(true, !false)
+  });
+});
