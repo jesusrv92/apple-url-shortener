63321e1287af28e63b5db45bf91149f24b738279
Jesus Romero <jhromero@tekmexico.com>
2021-01-26 11:54:53 -0800

    feat: added another test and autoformatting

--- test/functionality_test.js
+++ test/functionality_test.js
@@ -2,19 +2,23 @@ import assert from 'assert'
 
 describe('Functionality testing', () => {
   it('Should render page correctly', () => {
-      //TODO
-      assert.strictEqual(true, !false)
-    });
-    it('Should return shortened url', () => {
-      //TODO
-      assert.strictEqual(true, !false)
-    });
-    it('Should return same url if page already given', () => {
-      //TODO
-      assert.strictEqual(true, !false)
-    });
-    it('Should redirect to correct page', () => {
-      //TODO
+    //TODO
+    assert.strictEqual(true, !false)
+  });
+  it('Should return shortened url', () => {
+    //TODO
+    assert.strictEqual(true, !false)
+  });
+  it('Should return same url if page already given', () => {
+    //TODO
+    assert.strictEqual(true, !false)
+  });
+  it('Should redirect to correct page', () => {
+    //TODO
+    assert.strictEqual(true, !false)
+  });
+  it('Should return new value if expired', () => {
+    //TODO
     assert.strictEqual(true, !false)
   });
 });
